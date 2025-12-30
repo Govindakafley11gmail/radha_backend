af8d5622da3ae42dcfc992b376ad8bb600e99838
Govindakafley11gmail <govindakafley11@gmail.com>
2025-12-30 19:44:13 +0600

    added

--- src/modules/cost-accounting/ProductionCosting/labor/labor.module.ts
+++ src/modules/cost-accounting/ProductionCosting/labor/labor.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LaborService } from './labor.service';
 import { LaborController } from './labor.controller';
+import { Labor } from './entities/labor.entity';
+import { LaborCost } from '../labor-cost/entities/labor-cost.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Labor, LaborCost]), // 🔑 Register entities
+  ],
   controllers: [LaborController],
   providers: [LaborService],
 })
