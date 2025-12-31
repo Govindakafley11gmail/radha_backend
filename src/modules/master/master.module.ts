fc4ce2b7807351a84978f389cec3681fcf9cd001
Govindakafley11gmail <govindakafley11@gmail.com>
2025-12-31 11:13:36 +0600

    finally please fully

--- src/modules/master/master.module.ts
+++ src/modules/master/master.module.ts
@@ -3,8 +3,9 @@ import { BranchesModule } from './branches/branches.module';
 import { DepartmentsModule } from './department/department.module';
 import { AccountGroupModule } from './account_group/account_group.module';
 import { AccountTypesModule } from './account_types/account_types.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [BranchesModule, DepartmentsModule, AccountGroupModule, AccountTypesModule]
+  imports: [BranchesModule, DepartmentsModule, AccountGroupModule, AccountTypesModule, CategoryModule]
 })
 export class MasterModule {}
